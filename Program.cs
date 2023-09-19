6236dbd45aeb44442a9b9a932251d1b3ac52e45d
Rem Blankenburgh <rem.blankenburgh@gmail.com>
2023-09-19 10:14:23 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Repeat
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder().ConfigureServices((hostingContext, services) =>
+           {
+               services.AddHostedService<RepeatScheduler>();
+           });
+
+            await builder.RunConsoleAsync();
+        }
+
+    }
+}
---
+++ RepeatScheduler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Repeat
+{
+    internal class RepeatScheduler : BackgroundService
+    {
+        private const double HourInterval = 2.0 / 3600; // Two seconds for the example; most applications will need quite a higher interval, in the order of hours.
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Fire.");
+                // Do your thing here
+
+                await Task.Delay(TimeSpan.FromHours(HourInterval), stoppingToken);
+            }
+        }
+
+    }
+}
